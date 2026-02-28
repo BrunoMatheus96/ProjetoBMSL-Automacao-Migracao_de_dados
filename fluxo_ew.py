5c0f4914151a2185b357d15836c0178a9d95dd35
BrunoMatheus96 <brunomatheusstefanoleite@gmail.com>
2026-02-28 00:23:58 -0300

    ajustes

--- fluxo_ew.py
+++ fluxo_ew.py
@@ -18,3 +18,7 @@ def selec_planilha():
         print("Arquivo Excel selecionado:", file_path)
     else:
         print("O arquivo precisa ser do tipo Excel (.xlsx ou .xls).")
+
+
+def transformar_planilha(file_path):
+    print(f"Transformando a planilha {file_path}... (função ainda não implementada)")
