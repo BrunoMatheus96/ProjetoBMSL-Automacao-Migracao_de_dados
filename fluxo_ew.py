645b156453c2f152b05112d83009f39c7e1e20b4
BrunoMatheus96 <brunomatheusstefanoleite@gmail.com>
2026-02-28 00:03:55 -0300

    Criação do projeto

---
+++ fluxo_ew.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+from tkinter import filedialog
+
+
+def selec_planilha():
+    # Cria uma janela “root” escondida
+    root = tk.Tk()
+    root.withdraw()  # Esconde a janela principal
+
+    # Abre a janela para selecionar um arquivo
+    file_path = filedialog.askopenfilename(
+        title="Selecione um arquivo", filetypes=[("Todos os arquivos", "*.*")]
+    )
+
+    if file_path == "":
+        print("Nenhum arquivo selecionado.")
+    elif file_path.lower().endswith((".xlsx", ".xls")):
+        print("Arquivo Excel selecionado:", file_path)
+    else:
+        print("O arquivo precisa ser do tipo Excel (.xlsx ou .xls).")
